915b436d7b44b14c9d6548f04a86eb71c5d13d1f
noriaki watanabe <noriaki.watanabe@l-is-b.com>
2026-04-07 11:55:17 +0900

    Add Swift Package Manager support for v2.1.0

---
+++ ACEDrawingView/include/ACEDrawingView.h
@@ -0,0 +1,10 @@
+#ifndef ACEDrawingView_h
+#define ACEDrawingView_h
+
+#import <UIKit/UIKit.h>
+
+#import "ACEDrawingView/ACEDrawingView.h"
+#import "ACEDrawingView/ACEDrawingTools.h"
+#import "ACEDrawingView/ACEDrawingToolState.h"
+#import "ACEDrawingView/ACEDrawingLabelView.h"
+#endif /* ACEDrawingView_h */
